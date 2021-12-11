12a74bd4e927c99b3d60b2befcc93b376862593d
Ravish <gitwithravish@gmail.com>
2021-12-11 18:35:03 +0530

    updated fill method for documents

--- src/Traits/CanFillClassProperties.php
+++ src/Traits/CanFillClassProperties.php
@@ -2,14 +2,25 @@
 
 namespace Delta4op\MongoODM\Traits;
 
+use Exception;
+
 trait CanFillClassProperties
 {
+    /**
+     * Filss the object properties
+     *
+     * @throws Exception
+     */
     public function fill(array $attributes = []): static
     {
         foreach($attributes as $key => $value) {
 
+            if(!isset($value)) {
+                continue;
+            }
+
             if(! property_exists($this, $key)){
-                throw new \Exception("Property $key does not exist");
+                throw new Exception("Property $key does not exist");
             }
 
             $this->{$key} = $value;
